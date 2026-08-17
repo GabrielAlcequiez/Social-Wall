df6fb938dcf1d00adca33de84cde8e2bcf393a90
Gabriel Alcequiez <gxbbab@gmail.com>
2026-08-17 12:44:03 -0400

    feat: update routing with public home and protected create-post

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-/* eslint-disable no-unused-vars */
-import { useState } from "react";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import RegisterPage from "./pages/RegisterPage";
-import { Routes, Route, ProtectedRoute } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
-// Importaciones de modulos de Firebase 
-import appFirebase from "./services/firebaseConfig";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-const auth = getAuth(appFirebase);
+import CreatePostPage from "./pages/CreatePostPage";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoutes";
 
 function App() {
-
-
   return (
     <div>
       <Header />
-      
+
       <Routes>
-        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/create-post"
+          element={
+            <ProtectedRoute>
+              <CreatePostPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
